a1931c56fc5c2515d7cab05a5a8bfeb4f63f7094
aqeebrasool-del <aqeebrasool@gmail.com>
2026-06-02 16:00:27 +0530

    Update rg.tf

--- rg.tf
+++ rg.tf
@@ -1,7 +1,3 @@
-resource "azurerm_resource_group" "rgs1" {
-  name     = "rg-slovenia"
-  location = "westus"
-}
 
 resource "azurerm_resource_group" "rgsraqib112211" {
   name     = "rg-raqib112211"
